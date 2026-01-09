129a5873b2d61fba1c84c696eb103fa1b78827aa
Saidel Lopez <sailopher@yahoo.com.mx>
2026-01-08 20:53:12 -0600

    updated the home screen to show the items a little finer than just a text

--- app/src/main/java/com/saishaddai/composetraining/screens/HomeScreen.kt
+++ app/src/main/java/com/saishaddai/composetraining/screens/HomeScreen.kt
@@ -1,13 +1,26 @@
 package com.saishaddai.composetraining.screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -21,9 +34,34 @@ fun HomeScreen(navigateToScreen: (String) -> Unit) {
                             navigateToScreen(it.toString())
                         }
                 ) {
-                    Text("position $it")
+                    Card(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp, vertical = 8.dp)
+                            .fillMaxWidth(),
+                        shape = RoundedCornerShape(8.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Column(modifier = Modifier.weight(1f)) {
+                                Text("Title $it", fontSize = 18.sp)
+                                Text("this is a description for the item $it")
+                            }
+                            Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "")
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(navigateToScreen = {})
 }
