bedf5b78d99a245aa73880f89d496bfcee9a69c5
Saidel Lopez <sailopher@yahoo.com.mx>
2026-01-11 19:32:45 -0600

    finished with the business card

--- app/src/main/java/com/saishaddai/composetraining/screens/BusinessCardScreen.kt
+++ app/src/main/java/com/saishaddai/composetraining/screens/BusinessCardScreen.kt
@@ -4,14 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,12 +57,34 @@ fun BusinessCardScreen() {
             )
         }
 
-        Text(
-            text = "Test information in teh bottom of the screen", //stringResource(R.string.nice_work),
-            fontSize = 12.sp,
+        Column(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .padding(bottom = 24.dp)
+                .padding(bottom = 42.dp)
+        ) {
+            ContactItem(icon = Icons.Default.Phone, text = stringResource(R.string.business_card_text1))
+            ContactItem(icon = Icons.Default.Share, text = stringResource(R.string.business_card_text2))
+            ContactItem(icon = Icons.Default.Email, text = stringResource(R.string.business_card_text3))
+        }
+    }
+}
+
+@Composable
+fun ContactItem(modifier: Modifier = Modifier, icon: ImageVector, text: String) {
+    Row(
+        modifier = modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = ""
+        )
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .padding(start = 24.dp)
         )
     }
 }
