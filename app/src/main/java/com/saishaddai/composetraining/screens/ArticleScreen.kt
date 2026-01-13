4c4393ea7ab1ad38b82918ae84b794ba7c70ff1b
Saidel Lopez <sailopher@yahoo.com.mx>
2026-01-12 23:31:05 -0600

    fixed missing texts. It now uses strings.xml

--- app/src/main/java/com/saishaddai/composetraining/screens/ArticleScreen.kt
+++ app/src/main/java/com/saishaddai/composetraining/screens/ArticleScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -15,12 +16,13 @@ import com.saishaddai.composetraining.R
 
 @Composable
 fun ArticleScreen(
-    title: String,
-    articleAbstract: String,
-    articleText: String,
     modifier: Modifier = Modifier
 ) {
     val image = painterResource(R.drawable.bg_compose_background)
+    val title = stringResource(R.string.article_title)
+    val articleAbstract = stringResource(R.string.article_abstract_text)
+    val articleText = stringResource(R.string.article_abstract_text)
+
     Column(
         modifier = modifier
     ) {
--- app/src/main/java/com/saishaddai/composetraining/screens/BirthdayScreen.kt
+++ app/src/main/java/com/saishaddai/composetraining/screens/BirthdayScreen.kt
@@ -12,14 +12,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.saishaddai.composetraining.R
 
 @Composable
-fun BirthdayScreen(message: String, from: String, modifier: Modifier = Modifier) {
+fun BirthdayScreen(modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidparty)
+    val message = stringResource(R.string.happy_birthday_text)
+    val from = stringResource(R.string.signature_text)
     Box(modifier) {
         Image(
             painter = image,
