f8c6d2e315272637bcd9ba0b1c9f79bf51742c4e
Saidel Lopez <sailopher@yahoo.com.mx>
2026-01-20 23:41:42 -0600

    improved the error screen with a background image and manipulation of the colors of a Button to make it transparent

--- app/src/main/java/com/saishaddai/composetraining/screens/ErrorScreen.kt
+++ app/src/main/java/com/saishaddai/composetraining/screens/ErrorScreen.kt
@@ -1,34 +1,60 @@
 package com.saishaddai.composetraining.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.saishaddai.composetraining.R
 
 @Composable
 fun ErrorScreen(modifier: Modifier = Modifier, navigateToScreen: () -> Unit) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("No screen found"
-            , fontSize = 40.sp
-            , modifier = modifier.padding(bottom = 10.dp))
-        Button(
-            onClick = { navigateToScreen() }
+    val painter = painterResource(R.drawable.bg_red)
+    Box(modifier) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text(text = "Back",
-                fontSize = 26.sp,
-                modifier = modifier.padding(top = 10.dp, bottom = 10.dp))
+            Text(
+                "No screen found", fontSize = 40.sp, modifier = Modifier.padding(bottom = 10.dp)
+            )
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { navigateToScreen() },
+                colors = ButtonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White,
+                    disabledContainerColor = Color.Transparent,
+                    disabledContentColor = Color.White
+                )
+            ) {
+                Text(
+                    text = "Back",
+                    fontSize = 38.sp,
+                    modifier = Modifier.padding(top = 10.dp, bottom = 10.dp)
+                )
+            }
         }
     }
 }
