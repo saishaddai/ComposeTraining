8ce42230c7946848153c7bfe6ff03efe3c50e385
Saidel Lopez <sailopher@yahoo.com.mx>
2026-01-22 23:14:58 -0600

    added a new background image and set it into the Home Screen

--- app/src/main/java/com/saishaddai/composetraining/screens/HomeScreen.kt
+++ app/src/main/java/com/saishaddai/composetraining/screens/HomeScreen.kt
@@ -1,12 +1,17 @@
 package com.saishaddai.composetraining.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,50 +19,71 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.saishaddai.composetraining.R
 import com.saishaddai.composetraining.data.screens
 
 
 @Composable
 fun HomeScreen(navigateToScreen: (String) -> Unit) {
-    Scaffold { paddingValues ->
-        LazyColumn(Modifier.padding(paddingValues)) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.bg_blue_gradient),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        LazyColumn(
+            contentPadding = WindowInsets.systemBars.asPaddingValues()
+        ) {
             items(screens) { screen ->
                 Box(
-                    modifier = Modifier.clickable {
+                    modifier = Modifier
+                        .clickable {
                             navigateToScreen(screen.id)
-                        }) {
-                    Card(
-                        modifier = Modifier
-                            .padding(horizontal = 16.dp, vertical = 8.dp)
-                            .fillMaxWidth(), shape = RoundedCornerShape(8.dp)
-                    ) {
-                        Row(
-                            modifier = Modifier
-                                .padding(16.dp)
-                                .fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            Column(modifier = Modifier.weight(1f)) {
-                                Text(screen.title, fontSize = 18.sp)
-                                Text(screen.description)
-                            }
-                            Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "")
                         }
-                    }
+                ) {
+                    ScreenItem(screen.title, screen.description)
                 }
             }
         }
     }
 }
+
+@Composable
+fun ScreenItem(title: String, description: String) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(title, fontSize = 18.sp)
+                Text(description)
+            }
+            Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "")
+        }
+    }
+}
+
 
 @Preview
 @Composable
